85bb388ac8352a2abc3b52a1809c813dab038b76
Augusto Bernardo <gutooliveira027@gmail.com>
2022-02-14 00:42:18 -0300

    adicionando animações no html

--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
-/* Abre e fecha menu lateral em modo mobile*/
+// Abre e fecha menu lateral em modo mobile
 
 const menuMobile = document.querySelector('.menu-mobile');
 const body = document.querySelector('body');
@@ -13,7 +13,7 @@ menuMobile.addEventListener('click', () => {
 });
 
 
-/* Fecha o menu quando clicar em algum item e muda o icone para list */
+// Fecha o menu quando clicar em algum item e muda o icone para list 
 
 const navItem = document.querySelectorAll('.nav-item')
 
@@ -25,3 +25,29 @@ navItem.forEach(item => {
         }
     })
 })
+
+// Animar todos os itens na tela que tiverem o atributo data-anime
+
+const item = document.querySelectorAll("[data-anime"); //vai gerar um array para poder fazer um forEach
+
+const animeScroll = () => {
+    // inicialmente precisamos pegar qual o topo da página
+    const windowTop = window.pageYOffset + window.innerHeight * 0.85;
+    
+    // lê a altura do windowsTop e se a condição for verdadeira adiciona a classe "animate"
+    item.forEach(element => {
+        if (windowTop > element.offsetTop) {
+            element.classList.add("animate");
+        }else {
+            element.classList.remove("animate")
+        }
+    });
+
+}
+
+animeScroll();
+
+//pegando o scroll da tela
+window.addEventListener("scroll", () => {
+    animeScroll()
+});
