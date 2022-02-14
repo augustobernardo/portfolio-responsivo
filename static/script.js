55c56d81dcc1402a003d220e263d19190756227f
Augusto Bernardo <gutooliveira027@gmail.com>
2022-02-14 02:04:01 -0300

    botão de loading adicionado e alerta de sucesso do envio da mensagem

--- static/script.js
+++ static/script.js
@@ -52,3 +52,19 @@ animeScroll();
 window.addEventListener("scroll", () => {
     animeScroll()
 });
+
+// Ativar o carregamento no botão de enviar do formulário
+const btnEnviar = document.querySelector('#btn-enviar');
+const btnEnviarLoader = document.querySelector('#btn-enviar-loader');
+
+
+btnEnviar.addEventListener("click", () => {
+    btnEnviarLoader.style.display = "block";
+    btnEnviar.style.display = "none"
+})
+
+// Tira o alerta de sucesso do envio da mensagem
+setTimeout(() => {
+    document.querySelector("#alerta").style.display = 'none';
+
+}, 5000)
